a9ddf8a84e0d8fda0e0be7f4d5da1b5662096553
Lisbel Cruz <Lcruztaveras@gmail.com>
2019-01-07 18:34:39 -0600

    adding review skills

---
+++ review skills/rentCar_lp/gulpfile.js
@@ -0,0 +1,98 @@
+// --------------------------------------------
+// Dependencies
+// --------------------------------------------
+var autoprefixer = require('gulp-autoprefixer'),
+    concat = require('gulp-concat'),
+    del = require('del'),
+    gulp = require('gulp'),
+    minifycss = require('gulp-minify-css'),
+    plumber = require('gulp-plumber'),
+    sass = require('gulp-sass'),
+    sourcemaps = require('gulp-sourcemaps'),
+    rename = require('gulp-rename'),
+    uglify = require('gulp-uglify'),
+    images = require('gulp-imagemin'),
+    browserSync = require('browser-sync').create();
+
+
+// paths
+var styleSrc = 'source/sass/**/*.scss',
+    styleDest = 'build/css/',
+    htmlSrc = 'source/',
+    htmlDest = 'build/',
+    vendorSrc = 'source/js/vendors/',
+    vendorDest = 'build/js/',
+    scriptSrc = 'source/js/*.js',
+    scriptDest = 'build/js/';
+
+
+
+// --------------------------------------------
+// Stand Alone Tasks
+// --------------------------------------------
+
+
+// Compiles all SASS files
+gulp.task('sass', function() {
+    gulp.src('source/sass/**/*.scss')
+        .pipe(plumber())
+        .pipe(sass({
+            style: 'compressed'
+        }))
+        .pipe(rename({
+            basename: 'main',
+            suffix: '.min'
+          }))
+
+        .pipe(gulp.dest('build/css'));
+});
+
+gulp.task('images', function() {
+    gulp.src('source/img/*')
+        .pipe(images())
+        .pipe(gulp.dest('build/img'));
+});
+
+// Uglify js files
+gulp.task('scripts', function() {
+    gulp.src('source/js/*.js')
+        .pipe(plumber())
+        .pipe(uglify())
+        .pipe(gulp.dest('build/js'));
+});
+
+//Concat and Compress Vendor .js files
+gulp.task('vendors', function() {
+    gulp.src(
+            [
+                'source/js/vendors/*.js'
+            ])
+        .pipe(plumber())
+        .pipe(concat('vendors.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('build/js'));
+});
+
+
+
+// Watch for changes
+gulp.task('watch', function(){
+
+    // Serve files from the root of this project
+    browserSync.init({
+        server: {
+            baseDir: "./build"
+        },
+        notify: false
+    });
+
+    gulp.watch(styleSrc,['sass']);
+    gulp.watch(scriptSrc,['scripts']);
+    gulp.watch(vendorSrc,['vendors']);
+    gulp.watch(['build/*.html', 'build/css/*.css', 'build/js/*.js', 'build/js/vendors/*.js']).on('change', browserSync.reload);
+
+});
+
+
+// use default task to launch Browsersync and watch JS files
+gulp.task('default', [ 'sass', 'scripts', 'vendors', 'watch'], function () {});
