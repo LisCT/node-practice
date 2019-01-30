5d1919b1a7970588f92caf77467219a85d1164da
Lisbel Cruz <Lcruztaveras@gmail.com>
2019-01-29 19:05:38 -0600

    adding redirect

--- flash card/app.js
+++ flash card/app.js
@@ -1,23 +1,48 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 
 const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false}));
+app.use(cookieParser());
 
 app.set('view engine', 'pug');
 
 app.get('/', (req, res) => {
-    res.render('index');
+
+    const name = req.cookies.username;
+
+    // passing name from the cookies to avoid repeat it again
+    name ? res.render('index', { name }) : res.redirect('/hello');
+
 });
 
 app.get('/cards', (req, res) => {
+
     res.render('card', { 
         prompt: "who is buried in Grant's tomb?", hint: "Think about whose tomb it is."});
+
 });
 
 app.get('/hello', (req, res) => {
-    res.render('hello');
+
+    const name = req.cookies.username;
+
+    name ? res.redirect('/') : res.render('hello');
+
+});
+
+app.post('/hello', (req, res) => {
+
+    res.cookie('username', req.body.username); // save it in the cookies (set)
+    res.redirect('/');
+
 })
 
 app.listen(3000, () => {
+
     console.log("The application is runing on localhost:3000");
+    
 });
 
